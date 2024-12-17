e8c83243aa73a7b79b38916286ec9a1113f1ae57
Jose Tomas <lorite.borrego.jose.tomas@iescamas.es>
2024-12-17 20:25:34 +0100

    Modificación ameba

--- src/ejercicio4n/Main.java
+++ src/ejercicio4n/Main.java
@@ -5,9 +5,14 @@ public class Main {
 	public static void main(String[] args) {
 		Ameba ameba1 = new Ameba ();
 		Ameba ameba2 = new Ameba ();
+		Ameba ameba3 = new Ameba ();
+		Ameba ameba4 = new Ameba ();
 		
 		ameba1.come(ameba2);
 		ameba2.come(5);
+		ameba3.come(10);
+		ameba4.come(7);
+		
 		
 		System.out.println(ameba1.peso);
 		System.out.println(ameba2.peso);
